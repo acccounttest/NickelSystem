9d41c1bab29b703f9dff43c6fdbee35fa9db0c2e
acccounttest <1sitecode@gmail.com>
2021-08-02 19:39:25 +0200

    Create NickelSystem.py

---
+++ NickelSystem.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from mtTkinter import *
+#Entry,Label,Toplevel,StringVar,EW
+from os.path import getsize,normpath,isfile,isdir,islink
+import ctypes
+import thread
+import threading
+import time
+from collections import OrderedDict
+import subprocess
+from lib.ScrollB import ScrollB
+
+from lib.Rtfd import Rtfd
+
+from lib.Binding import Binding as Binding
+
+from os import path
+
+#Script comes here , only import steps are a little abstract waiting the code.
